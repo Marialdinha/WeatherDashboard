a52fe5ead068c9957677ebaf2681f8b2ee2289d8
Marialdinha <marialda@bellsouth.net>
2023-05-29 17:40:08 -0500

    Added HTML elements

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var APIKey = "c78deab59c2355453ce985b6f36c6f2d";
+var city;
+
+
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={APIKey} // <-- 5 days
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={APIKey} // <-- current 
