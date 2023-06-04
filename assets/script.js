a84480649084133733c3ec110ed545cae669a7d2
Marialdinha <marialda@bellsouth.net>
2023-06-04 17:30:36 -0500

    Added save and retrieve from local storage

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,94 @@
 // our variables
 var btnSearch = $("#search");
 var cityToSearch = $("#city-to-search");
+var enterCity =  $("#enter-city");
+var cityDisplay = $("#city-display");
 var apiKey = "c78deab59c2355453ce985b6f36c6f2d";
 var requestedCity;
+
+// localStorage.clear();
 
 
 //Initialization
 function init(){
-    $("#enter-city").hide();
+    cityDisplay.hide();
     cityToSearch.text = "";
     requestedCity = "";
+    retrieveCityHistory()
 }
 
 
 // Event Listener for search button
  function setEventListeners(){
     btnSearch.on("click", function(){
-        
+        cityWeather();   
+      })
+    }
+
+
+// 
+async function cityWeather(){ 
     requestedCity = cityToSearch.val();
+    requestedCity = capitalizeFirstLetter(requestedCity);
+
     // checking if NO city was entered
     if (requestedCity === null || requestedCity === ""){
-        $("#enter-city").show();
+        enterCity.show();
         setTimeout(() => {
-            $("#enter-city").hide();
+            enterCity.hide();
         }, 4000);
     }else{
-        retrieveWeather();
-    }    
-      })
+        saveCityHistory();
+
+        // getting current weather info
+        // waiting for API call to finalize
+        await retrieveCurrentWeather().then((resp) => { 
+            return resp.json();
+        })
+        .then((data) => {        
+        console.log(data.weather[0].description);
+        })
+    }
+}
+    
+ async function retrieveCurrentWeather(){
+    const apiCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + requestedCity + "&appid=" + apiKey ;
+    const getCurrentWeather = fetch(apiCurrentWeather);
+    return(getCurrentWeather);
+ }
+
+
+ async function retrieve5DaysWeather(){
+    const api5DaysWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + requestedCity + "&appid=" + apiKey ;
+    // const api5DaysWeather = "https://api.github.com/repos/IBM/clai/issues?per_page=5";
+    const get5DaysWeather = fetch(api5DaysWeather);
+    return(get5DaysWeather);
+}
+
+ // Define function to capitalize the first letter of a string
+function capitalizeFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+// saving requested cities in local storage
+function saveCityHistory(){
+    if (localStorage.getItem(requestedCity) === null){
+        localStorage.setItem(requestedCity, requestedCity);
+    }
 }
 
 
-async function retrieveWeather(){
-        const apiCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q={" + requestedCity + "}&appid={" + apiKey + "}";
-        const api5DaysWeather = "https://api.openweathermap.org/data/2.5/forecast?q={" + requestedCity + "}&appid={" + apiKey + "}";
-        console.log(api5DaysWeather);
-        console.log(apiCurrentWeather);
-        const getCurrentWeather = fetch(apiCurrentWeather);
-    
+// retrieving city history from local storage
+function retrieveCityHistory(){
+
+    if (localStorage.length > 0) {
+        cityDisplay.empty()
+        for(index = 0; index < localStorage.length; index++){
+            var cityKey = localStorage.key(index);
+            cityDisplay.append('<div>'+ localStorage.getItem(cityKey)  + '</div>')
+        }
+        cityDisplay.show();
+    }
 }
 
 
@@ -46,19 +97,3 @@ $(document).ready(function(){
     init();
     setEventListeners();
 })
-
-
-//  creatin HTML dinamically
-// let html ="";
-// for(index = 0; index < highscores.length; index++){
-//     html += '<div> '+ highscores[index].name + " scored: " + highscores[index].score + '</div>'
-// }
-// highScore.innerHTML= html;
-// highScore.style.display = "initial";
-
-
-
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={APIKey} // <-- 5 days
-
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={APIKey} // <-- current 
-
