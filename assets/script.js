d896b307b8122ddf1e1846bae0ded252ed8b9e65
Marialdinha <marialda@bellsouth.net>
2023-06-09 23:48:48 -0500

    Retrieving cities from local storage

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,13 @@ var btnSearch = $("#search");
 var cityToSearch = $("#city-to-search");
 var enterCity =  $("#enter-city");
 var cityDisplay = $("#city-display");
+var currentCityName = $("#curent-city-name");
+var currentTemp = $("#curent-Temp");
+var currentWind = $("#curent-Wind");
+var currentHumidity = $("#curent-Humidity");
 var apiKey = "c78deab59c2355453ce985b6f36c6f2d";
 var requestedCity;
-
-// localStorage.clear();
+var cityKey;
 
 
 //Initialization
@@ -14,7 +17,7 @@ function init(){
     cityDisplay.hide();
     cityToSearch.text = "";
     requestedCity = "";
-    retrieveCityHistory()
+    initializeCityHistory()
 }
 
 
@@ -28,67 +31,107 @@ function init(){
 
 // 
 async function cityWeather(){ 
-    requestedCity = cityToSearch.val();
-    requestedCity = capitalizeFirstLetter(requestedCity);
+    requestedCity =  capitalizeFirstLetter(cityToSearch.val());
 
     // checking if NO city was entered
     if (requestedCity === null || requestedCity === ""){
-        enterCity.show();
+        cityToSearch.prop("disabled",true);
+        cityToSearch.val("Please, enter a city name to be searched");
         setTimeout(() => {
-            enterCity.hide();
-        }, 4000);
+            cityToSearch.val("");
+            cityToSearch.prop("disabled",false);
+        }, 1000);
     }else{
-        saveCityHistory();
-
         // getting current weather info
         // waiting for API call to finalize
         await retrieveCurrentWeather().then((resp) => { 
             return resp.json();
         })
-        .then((data) => {        
-        console.log(data.weather[0].description);
+        .then((data) => {      
+        currentCityName.text(data.name);
+        currentTemp.text(data.main.temp);
+        currentWind.text(data.wind.deg);
+        currentHumidity.text(data.main.humidity);
+       
+        // saving city if weather was retrieved correctly
+        saveCityHistory();
+        initializeCityHistory();
         })
+
+  
+        // getting 5 days weather info
+        // waiting for API call to finalize
+        await retrieve5DaysWeather().then((resp) => { 
+            return resp.json();
+        })
+        .then((data) => {    
+        })  
     }
 }
     
+
+// API call for current weather
  async function retrieveCurrentWeather(){
-    const apiCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + requestedCity + "&appid=" + apiKey ;
+    const apiCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + requestedCity + "&units=imperial&appid=" + apiKey ;
     const getCurrentWeather = fetch(apiCurrentWeather);
     return(getCurrentWeather);
  }
 
 
+// API call for 5 days weather
  async function retrieve5DaysWeather(){
-    const api5DaysWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + requestedCity + "&appid=" + apiKey ;
-    // const api5DaysWeather = "https://api.github.com/repos/IBM/clai/issues?per_page=5";
+    const api5DaysWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + requestedCity + "&units=imperial&appid=" + apiKey ;``
     const get5DaysWeather = fetch(api5DaysWeather);
     return(get5DaysWeather);
 }
 
- // Define function to capitalize the first letter of a string
+
+ // Function to capitalize the first letter of a string
 function capitalizeFirstLetter(string){
+    string = string.trim();
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
 // saving requested cities in local storage
 function saveCityHistory(){
+    // setting city in local storage
     if (localStorage.getItem(requestedCity) === null){
+        // setting limit to hto the number of cities stored 
+        if (localStorage.length > 2) {
+            cityKey = localStorage.key(3);
+            localStorage.removeItem(cityKey); 
+        }
         localStorage.setItem(requestedCity, requestedCity);
-    }
+}
 }
 
 
-// retrieving city history from local storage
-function retrieveCityHistory(){
+// initialize city's history from local storage
+function initializeCityHistory(){
+
+    // initializing buttons with cities from local storage
+    console.log(cityDisplay.length);
+    console.log(cityDisplay.data);
+    console.log(localStorage);
 
     if (localStorage.length > 0) {
-        cityDisplay.empty()
+        for(index = cityDisplay.length; index < 1; index--){}
+        cityKey = localStorage.key(index);
+            cityDisplay.remove(cityKey);
         for(index = 0; index < localStorage.length; index++){
-            var cityKey = localStorage.key(index);
-            cityDisplay.append('<div>'+ localStorage.getItem(cityKey)  + '</div>')
+        cityKey = localStorage.key(index);
+        cityDisplay.append(`<button id="${cityKey}" class="btn">${cityKey}</button>\n`);
         }
         cityDisplay.show();
-    }
+
+        //add event lsitner to all city buttons
+        cityDisplay.each(function(){
+            $(this).children("button").on("click",function(event){
+            event.preventDefault();
+            cityToSearch.val($(this).attr("id"));
+            cityWeather();  
+         })
+      })
 }
 
 
